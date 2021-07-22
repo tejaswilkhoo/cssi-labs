97a51af985598e9d8ab257ca05c6adcaaf9a7801
Tejas Wilkhoo <wilkhoo.tejas@gmail.com>
2021-07-22 22:13:53 +0000

    Completed the medium tasks for day 09

--- day09/starter/secretMessageApp/public/js/writeMessage.js
+++ day09/starter/secretMessageApp/public/js/writeMessage.js
@@ -1,20 +1,51 @@
+function isUpper(str) {
+    return /[A-Z]/.test(str);
+}
+
+function isNumber(str) {
+    return /[0-9]/.test(str);
+}
+
 const submitMessage = () => {
     console.log('Submitting message...');
-    const passcodeInput = document.querySelector('#passcode');
-    const messageInput = document.querySelector('#message');
-    const passcodeValue = passcodeInput.value;
-    const messageValue = messageInput.value;
+    const inputLength = document.querySelector('#message').value.length;
+    const passValue = document.querySelector('#passcode').value;
+    console.log(inputLength);
+    console.log(passValue);
+    console.log(isUpper(passValue));
 
-    //Send to firebase
-    firebase.database().ref().push({
-        message: messageValue,
-        passcode: passcodeValue
-    });
+    if (inputLength > 10) {
+        alert("Your message is too long.");
+    }
 
-    // Clear values from input
-    passcodeInput.value = '';
-    messageInput.value = '';
+    else if (!isUpper(passValue)) {
+        alert("Please add a capital letter to your password.");
+    }
+
+    else if (!isNumber(passValue)) {
+        alert("Please add a number to your password.");
+    }
+
+    else {
+
+        const passcodeInput = document.querySelector('#passcode');
+        const messageInput = document.querySelector('#message');
+        const passcodeValue = passcodeInput.value;
+        const messageValue = messageInput.value;
+
+        //Send to firebase
+        firebase.database().ref().push({
+            message: messageValue,
+            passcode: passcodeValue
+        });
+
+        // Clear values from input
+        passcodeInput.value = '';
+        messageInput.value = '';
+    }
 };
+
+    
 
 // const sendMessageButton = document.querySelector('.button');
 // console.log(sendMessageButton);
